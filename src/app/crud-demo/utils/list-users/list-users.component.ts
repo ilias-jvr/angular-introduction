c4e47f5d88bb558e5b595217b7db0412c957b154
ilias-jvr <tzavilias@gmail.com>
2023-10-24 19:04:32 +0300

    update

---
+++ src/app/crud-demo/utils/list-users/list-users.component.ts
@@ -0,0 +1,22 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AppService } from 'src/app/app.service';
+import { Person } from 'src/app/interfaces/person';
+
+@Component({
+  selector: 'app-list-users',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './list-users.component.html',
+  styleUrls: ['./list-users.component.css'],
+})
+export class ListUsersComponent implements OnInit {
+  users: Person[] = [];
+  constructor(private appService: AppService = Inject(AppService)) {}
+
+  ngOnInit(): void {
+    this.appService.getAllUsers().subscribe((users) => {
+      this.users = users;
+    });
+  }
+}
